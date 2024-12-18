a9cd1528638badae8c845d19ccab170084e29a69
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2024-12-18 21:37:06 +0000

    fix: fix responsiveness issues in smaller screens

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex h-screen dark:bg-gray-900">
-    <div class="w-1/6">
+    <div class="w-1/6 md:w-1/5">
       <Sidebar />
     </div>
-    <div class="w-5/6 flex flex-col">
+    <div class="w-5/6 md:w-4/5 flex flex-col">
       <main class="flex-1 p-4">
         <RouterView />
       </main>
