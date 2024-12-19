6a11de610c73305efe3ff5c93360fd6e135d5108
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2024-12-19 22:22:37 +0000

    feat: add more language stuff for the settings page

--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="p-4">
-        <h1 class="text-2xl font-bold">Settings</h1>
+        <h1 class="text-2xl font-bold">{{ $t('pages.settings') }}</h1>
         <div class="flex flex-col mt-4">
-            <h3 class="text-lg font-bold">Language</h3>
+            <h3 class="text-lg font-bold">{{ $t('settings.language') }}</h3>
             <LanguageSwitch :languages="['de', 'en']" :changeLanguage="changeAppLanguage" />
         </div>
     </div>
