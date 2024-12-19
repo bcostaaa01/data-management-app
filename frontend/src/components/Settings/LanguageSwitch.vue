b10f0f381783206de4a1ba7f1d75585f9730873d
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2024-12-19 22:19:30 +0000

    feat: define language switch component for the settings, add EN and DE icons

---
+++ frontend/src/components/Settings/LanguageSwitch.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex space-x-10 p-2">
+        <div v-for="language in languages" :key="language">
+            <img :src="languageIcons[language as Language]" class="w-10 h-10 cursor-pointer"
+                @click="changeLanguage(language)" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import deIcon from "../../assets/de.svg";
+import enIcon from "../../assets/en.svg";
+
+type Language = "de" | "en";
+
+const languageIcons: Record<Language, string> = {
+    de: deIcon,
+    en: enIcon,
+};
+
+interface Props {
+    languages: Language[];
+    changeLanguage: (lang: Language) => void;
+}
+
+defineProps<Props>();
+</script>
--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>
-        <h1>Settings</h1>
-        <p>This is the settings page. Here you can configure various aspects of the application.</p>
+    <div class="p-4">
+        <h1 class="text-2xl font-bold">Settings</h1>
+        <div class="flex flex-col mt-4">
+            <h3 class="text-lg font-bold">Language</h3>
+            <LanguageSwitch :languages="['de', 'en']" :changeLanguage="changeAppLanguage" />
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import LanguageSwitch from '../components/Settings/LanguageSwitch.vue';
+import i18n from '../i18n/config';
+
+const changeAppLanguage = (lang: "de" | "en") => {
+    i18n.global.locale = lang;
+};
 </script>
