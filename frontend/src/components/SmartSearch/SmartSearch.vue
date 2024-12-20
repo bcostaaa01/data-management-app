9677f56fe544adb4ef02fd48d15cdd9001ad1ef5
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2024-12-20 22:10:55 +0000

    feat: display command dynamically based on user system

--- frontend/src/components/SmartSearch/SmartSearch.vue
+++ frontend/src/components/SmartSearch/SmartSearch.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div @click="openModal" tabindex="0" class="relative w-50 cursor-pointer">
-            <FwbInput type="text" placeholder="Search...(Cmd+K)" readonly
+            <FwbInput type="text" :placeholder="`Search...(${systemKey}+K)`" readonly
                 class="w-full py-2 text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer hover:bg-gray-800 transition-colors duration-200" />
         </div>
 
@@ -14,8 +14,9 @@
                 </div>
             </template>
             <template #body>
-                <ul v-if="results.length" class="divide-y divide-gray-200 text-sm">
-                    <li v-for="result in results" :key="result.id" class="px-4 py-2 cursor-pointer hover:bg-gray-100">
+                <ul v-if="results.length" class="text-sm">
+                    <li v-for="result in results" :key="result.id"
+                        class="px-2 py-2 cursor-pointer hover:bg-gray-800 rounded-md">
                         {{ result.name }}
                     </li>
                 </ul>
@@ -40,7 +41,11 @@ interface SearchResult {
 
 const isOpen = ref(false);
 const query = ref('');
-const results = ref<SearchResult[]>([]);
+const results = ref<SearchResult[]>([
+    { id: 1, name: 'Result 1' },
+    { id: 2, name: 'Result 2' },
+    { id: 3, name: 'Result 3' },
+]);
 
 const openModal = () => {
     isOpen.value = true;
@@ -51,6 +56,8 @@ const closeModal = () => {
     query.value = '';
     results.value = [];
 };
+
+const systemKey = ref(navigator.platform.includes('Mac') ? '⌘' : 'Ctrl');
 
 const handleKeyPress = (event: KeyboardEvent) => {
     if ((event.metaKey || event.ctrlKey) && event.key === 'k') {
