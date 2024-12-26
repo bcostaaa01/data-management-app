cc402e70741bea01d069eca6022edd04386e4558
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2024-12-26 23:09:18 +0000

    feat: improve magic link sent step content

--- frontend/src/views/Auth/SignIn.vue
+++ frontend/src/views/Auth/SignIn.vue
@@ -17,8 +17,9 @@
         </div>
 
         <div v-if="isMagicLinkRequested" class="text-center text-white py-8 text-sm flex flex-col items-center">
-            <FontAwesomeIcon :icon="faEnvelope" class="h-12 w-12 text-white" />
+            <FontAwesomeIcon :icon="faEnvelope" class="h-12 w-12 text-white mb-4" />
             <h1 class="text-2xl font-bold mb-4">Magic Link sent to your email.</h1>
+            <p class="text-sm text-gray-400">Open your email and click the link to sign in.</p>
         </div>
     </div>
 </template>
