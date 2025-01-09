ae3bdce892a325296ef642d45730d767cab0bf25
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-01-09 22:42:28 +0100

    feat: implement data table pagination component, table UI improvements

--- frontend/src/components/DataTable/DataTable.vue
+++ frontend/src/components/DataTable/DataTable.vue
@@ -1,16 +1,35 @@
 <template>
-    <div class="w-full">
-        <DataTable :value="items" :columns="columns">
+    <div class="bg-gray-800 w-full p-2 rounded-lg">
+        <DataTable selection-mode="single" :value="paginatedItems" :columns="columns" :rows="rows">
             <Column v-for="column in columns" :field="column.field" :header="column.header" />
         </DataTable>
+        <DataTablePagination :total-records="items.length" :rows="rows" :current-page="currentPage"
+            @page-change="onPageChange" />
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
+import DataTablePagination from './DataTablePagination.vue';
 
-defineProps<{
+const items = computed(() => props.items);
+const columns = computed(() => props.columns);
+const rows = ref(5);
+const currentPage = ref(1);
+
+const paginatedItems = computed(() => {
+    const start = (currentPage.value - 1) * rows.value;
+    const end = start + rows.value;
+    return items.value.slice(start, end);
+});
+
+const onPageChange = (page: number) => {
+    currentPage.value = page;
+};
+
+const props = defineProps<{
     items: any[];
     columns: any[];
 }>();
---
+++ frontend/src/components/DataTable/DataTablePagination.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex justify-center mt-2 w-full">
+        <FwbPagination show-icons enable-first-and-last-buttons :per-page="rows" :total-items="totalRecords"
+            @page-change="onPageChange" v-on:page-changed="onPageChange" v-model="currentPage" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps, defineEmits } from 'vue';
+import { FwbPagination } from 'flowbite-vue';
+
+const props = defineProps<{
+    totalRecords: number;
+    rows: number;
+    currentPage: number;
+}>();
+
+const currentPage = computed(() => props.currentPage);
+
+const emit = defineEmits(['page-change']);
+
+const onPageChange = (page: number) => {
+    console.log("Page changed to:", page);
+    emit('page-change', page);
+};
+</script>
