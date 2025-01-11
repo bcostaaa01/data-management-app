53c239bd6f38bca9b470c75da0a38e6e6dd554c5
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-01-11 17:09:14 +0100

    feat: UI improvements in the table connection, add more translations

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.p6bbgoij6p"
+    "revision": "0.3ari2pep4t"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/src/components/Data/Integrations/TableImport.vue
+++ frontend/src/components/Data/Integrations/TableImport.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="w-full">
-        <TableSearch v-if="!isConnecting" class="my-4 w-32" @update:value="updateTableName" />
-        <fwb-button class="mt-4" @click="importTable" :disabled="!tableName">
-            <span v-if="!isConnecting">{{ t('tables.importTable') }}</span>
-            <FontAwesomeIcon :icon="faSpinner" spin v-else />
-        </fwb-button>
+
+        <span class="flex my-3 font-bold">{{ t("tables.importTableInfo") }}</span>
+
+        <div class="flex flex-row items-center">
+            <TableSearch :disabled="isConnecting" class="my-4 w-32" @update:value="updateTableName" />
+            <FontAwesomeIcon :icon="faSquareCaretRight" class="m-5" />
+            <fwb-button @click="importTable" :disabled="!tableName">
+                <span v-if="!isConnecting">{{ t('tables.importTable') }}</span>
+                <div class="flex flex-row items-center" v-else>
+                    <span>{{ t("tables.importTableLoading") }}</span>
+                    <FontAwesomeIcon :icon="faSpinner" spin class="ml-2" />
+                </div>
+            </fwb-button>
+        </div>
     </div>
 </template>
 
@@ -14,7 +23,7 @@ import { FwbButton } from 'flowbite-vue';
 import { useI18n } from 'vue-i18n';
 import { useTablesStore } from '../../../stores/tables.store';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner, faSquareCaretRight } from '@fortawesome/free-solid-svg-icons';
 import TableSearch from './TableSearch.vue';
 
 const { t } = useI18n();
