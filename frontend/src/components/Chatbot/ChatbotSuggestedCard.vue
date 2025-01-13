5e6147474529f5781cf25ac8cd8ac0e207db473c
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-01-13 16:45:19 +0100

    feat: improve margins of the chatbot suggested cards

--- frontend/src/components/Chatbot/ChatbotSuggestedCard.vue
+++ frontend/src/components/Chatbot/ChatbotSuggestedCard.vue
@@ -2,7 +2,7 @@
     <div
         class="bg-white dark:bg-gray-900 rounded-lg border-gray-300 p-2 flex items-center justify-between transition-transform duration-300 hover:scale-105 cursor-pointer">
         <div class="flex items-center">
-            <span class="mr-2">
+            <span class="mx-2">
                 <FontAwesomeIcon :icon="icon" class="mr-2 bg-yellow-100 dark:bg-gray-700 rounded-full p-2" />
             </span>
             <fwb-card-title class="text-gray-800 dark:text-gray-300 text-sm">
