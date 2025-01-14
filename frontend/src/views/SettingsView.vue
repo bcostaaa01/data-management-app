3acaa980ff7fb22df8e75ff5cebb0c420f400ce1
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-01-14 23:28:09 +0100

    feat: remove padding from settings page

--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -1,12 +1,10 @@
 <template>
-    <div class="p-4">
-        <header class="flex justify-between items-center border-b border-gray-300 pb-4">
-            <h1 class="text-2xl font-bold dark:text-white">{{ t('pages.settings') }}</h1>
-        </header>
-        <div class="flex flex-col mt-4">
-            <h3 class="text-lg font-bold">{{ t('settings.language') }}</h3>
-            <LanguageSwitch :languages="languages" :changeLanguage="changeAppLanguage" />
-        </div>
+    <header class="flex justify-between items-center border-b border-gray-300 pb-4">
+        <h1 class="text-2xl font-bold dark:text-white">{{ t('pages.settings') }}</h1>
+    </header>
+    <div class="flex flex-col mt-4">
+        <h3 class="text-lg font-bold">{{ t('settings.language') }}</h3>
+        <LanguageSwitch :languages="languages" :changeLanguage="changeAppLanguage" />
     </div>
 </template>
 
