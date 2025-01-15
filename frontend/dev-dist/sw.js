11cd2440fa7a1ac58c2357810767564073974af7
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-01-15 18:09:41 +0100

    feat: sw updates

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.pddptlntv48"
+    "revision": "0.dajctdg97c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
