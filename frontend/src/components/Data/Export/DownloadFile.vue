c5b9775fadff01e722193d6b8b10b7c7aa281c0e
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-01-27 15:28:33 +0100

    feat: show loading spinner on click to download file

--- frontend/src/components/Data/Export/DownloadFile.vue
+++ frontend/src/components/Data/Export/DownloadFile.vue
@@ -1,12 +1,14 @@
 <template>
     <button @click="handleDownload" class="transition-all duration-300 hover:scale-110">
-        <FontAwesomeIcon :icon="faDownload" class="dark:text-white hover:text-gray-800" />
+        <FontAwesomeIcon v-if="!isLoading" :icon="faDownload" class="dark:text-white hover:text-gray-800" />
+        <FontAwesomeIcon v-else :icon="faSpinner" class="animate-spin" />
     </button>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { useOpenFile } from '../../../composables/useOpenFile';
 
 const { downloadFile } = useOpenFile();
@@ -15,11 +17,15 @@ const props = defineProps<{
     fileName: string;
 }>();
 
+const isLoading = ref(false);
+
 const handleDownload = async () => {
+    isLoading.value = true;
     const blob = await downloadFile(props.fileName);
     if (blob) {
         const url = URL.createObjectURL(blob);
         window.open(url, '_blank');
     }
+    isLoading.value = false;
 };
 </script>
