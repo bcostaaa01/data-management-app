4d24f4b6fc0a389c29efff7b4d818ea9b726824d
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-01-27 15:23:54 +0100

    refactor: formatting

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -2,7 +2,7 @@
     <header class="flex justify-between items-center border-b border-gray-300 pb-4">
         <h1 class="text-2xl font-bold dark:text-white">{{ t('dashboard.title') }}</h1>
     </header>
-
+    
     <span class="flex my-3 font-bold text-gray-500 dark:text-gray-100">{{ t("dashboard.dashboardInfo") }}</span>
 
     <div class="flex-1 justify-start mt-10 w-full mr-6">
@@ -15,6 +15,7 @@ import { onMounted } from 'vue';
 import { useGetFilesFromBucket } from '../composables/useGetFiles';
 import { useI18n } from 'vue-i18n';
 import FileAccordion from '../components/Dashboard/FileAccordion.vue';
+
 
 const { t } = useI18n();
 const { files, getFiles, fileIds } = useGetFilesFromBucket();
